c32689739874a1cb5db3a7f154a4ac6d720490d9
João Pedro Nolasco <nolascojoaops@gmail.com>
2025-01-11 16:50:17 -0300

    Redirect AWS CLI install output to /dev/null and re-start workflow

--- scripts/userdata.sh
+++ scripts/userdata.sh
@@ -11,13 +11,14 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
 sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo usermod -aG docker ubuntu
+#sudo usermod -aG docker ubuntu
+sudo usermod -aG docker admin
 
 # --------------------
 # Instalar AWS CLI
 # --------------------
 sudo apt install -y unzip
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
+unzip awscliv2.zip > /dev/null
+sudo ./aws/install > /dev/null
 rm -rf awscliv2.zip aws/

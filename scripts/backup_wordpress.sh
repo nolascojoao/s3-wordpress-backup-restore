d1fb13ce5366ce19b9c4255c8c1ed6a3c2261c5b
João Pedro Nolasco <nolascojoaops@gmail.com>
2025-01-11 12:26:58 -0300

    Add log output to scripts [skip ci]

--- scripts/backup_wordpress.sh
+++ scripts/backup_wordpress.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+mkdir -p /opt/backups/logs
+exec > >(tee -a /opt/backups/logs/backup.log) 2>&1
+
 # Carrega variáveis do .env
 set -a
 source /home/ubuntu/s3-wordpress-backup-restore/.env
@@ -9,17 +12,13 @@ set +a
 
 BACKUP_DIR="/opt/backups"
 FILES_BACKUP="wordpress_files_backup_$(date +%Y%m%d).tar.gz"
-
 DB_BACKUP="wp_db_backup_$(date +%Y%m%d).sql"
 S3_BUCKET="joaonolasco-wp-backup"
-
 WP_VOLUME_PATH="/var/lib/docker/volumes/s3-wordpress-backup-restore_wordpress_data/_data"
-
-mkdir -p $BACKUP_DIR/logs
 
 # Cria o .my.cnf dentro do container MySQL para permitir que o host se autentique com o banco de dados
 if ! docker exec mysql test -f /root/.my.cnf; then
-    echo -e "[client]\nuser=root\npassword=${MYSQL_ROOT_PASSWORD}" | docker exec -i mysql tee /root/.my.cnf > /dev/null
+    echo -e "[client]\nuser=root\npassword=${MYSQL_ROOT_PASSWORD}" | docker exec -i mysql tee /root/.my.cnf > /dev/n>
 fi
 
 # DB backup
@@ -35,4 +34,4 @@ aws s3 cp $BACKUP_DIR/$FILES_BACKUP s3://$S3_BUCKET/$FILES_BACKUP
 rm -f $BACKUP_DIR/$FILES_BACKUP
 rm -f $BACKUP_DIR/$DB_BACKUP
 
-echo "Backup completo e enviado para o S3!"
+echo "Backup completo e enviado para o S3! $(date '+%Y-%m-%d %H:%M:%S')"
--- scripts/restore_wordpress.sh
+++ scripts/restore_wordpress.sh
@@ -2,28 +2,27 @@
 
 set -e
 
+mkdir -p /opt/backups/logs
+exec > >(tee -a /opt/backups/logs/restore.log) 2>&1
+
 # Carrega variáveis do .env
 set -a
 source /home/ubuntu/s3-wordpress-backup-restore/.env
 set +a
 
-#BACKUP_DATE=$(date +%Y%m%d)	# Data de hoje (padrão)
-BACKUP_DATE=20250110		# Substitua por uma data específica no formato YYYYMMDD
-BACKUP_DIR="/opt/backups"
+BACKUP_DATE=$(date +%Y%m%d)     # Data de hoje (padrão)
+#BACKUP_DATE=20250112           # Substitua por uma data específica no formato YYYYMMDD
 
+BACKUP_DIR="/opt/backups"
 FILES_BACKUP="wordpress_files_backup_${BACKUP_DATE}.tar.gz"
 DB_BACKUP="wp_db_backup_${BACKUP_DATE}.sql"
-
 S3_BUCKET="joaonolasco-wp-backup"
-
 WP_VOLUME_PATH="/var/lib/docker/volumes/s3-wordpress-backup-restore_wordpress_data/_data"
 DB_VOLUME_PATH="/var/lib/docker/volumes/s3-wordpress-backup-restore_db_data/_data"
 
-mkdir -p $BACKUP_DIR/logs
-
 # Cria o .my.cnf dentro do container MySQL para permitir que o host se autentique com o banco de dados
 if ! docker exec mysql test -f /root/.my.cnf; then
-    echo -e "[client]\nuser=root\npassword=${MYSQL_ROOT_PASSWORD}" | docker exec -i mysql tee /root/.my.cnf > /dev/null
+    echo -e "[client]\nuser=root\npassword=${MYSQL_ROOT_PASSWORD}" | docker exec -i mysql tee /root/.my.cnf > /dev/n>
 fi
 
 # Restauração do banco de dados
@@ -38,4 +37,4 @@ tar -xzvf $BACKUP_DIR/$FILES_BACKUP -C $WP_VOLUME_PATH
 rm -f $DB_VOLUME_PATH/$DB_BACKUP
 rm -f $BACKUP_DIR/$FILES_BACKUP
 
-echo "Restauração do backup concluída!"
+echo "Restauração do backup concluída! $(date '+%Y-%m-%d %H:%M:%S')"

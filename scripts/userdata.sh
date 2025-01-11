2c1a56383e02cb0258e5bf1b502936ea5598fc3e
João Pedro Nolasco <nolascojoaops@gmail.com>
2025-01-11 17:07:17 -0300

    Rollback userdata script to work with Ubuntu

--- scripts/userdata.sh
+++ scripts/userdata.sh
@@ -6,27 +6,12 @@ set -e
 # --------------------
 sudo apt update
 sudo apt install -y ca-certificates curl gnupg
-
-# Adicionar chave GPG do Docker
 sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-
-# Adicionar o repositório Docker conforme o sistema (Ubuntu ou Debian)
-if grep -q "Ubuntu" /etc/os-release; then
-  echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-elif grep -q "Debian" /etc/os-release; then
-  echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-else
-  echo "Sistema não suportado para instalação automática do Docker."
-  exit 1
-fi
-
-# Atualizar e instalar o Docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
 sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-# Adicionar o usuário ao grupo docker
-sudo usermod -aG docker admin
+sudo usermod -aG docker ubuntu
 
 # --------------------
 # Instalar AWS CLI

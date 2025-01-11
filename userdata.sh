f4e6bd9d0bdb98d2946572ab686d6c7a55ba34a5
João Pedro Nolasco <nolascojoaops@gmail.com>
2025-01-11 09:05:18 -0300

    Add User Data script for Docker and AWS CLI setup

---
+++ userdata.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+# --------------------
+# Instalar Docker
+# --------------------
+sudo apt update
+sudo apt install -y ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo usermod -aG docker $USER
+
+# --------------------
+# Instalar AWS CLI
+# --------------------
+sudo apt install -y unzip
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+rm -rf awscliv2.zip aws/
